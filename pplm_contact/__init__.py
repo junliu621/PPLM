5c5b5efd5497f444cadda5b4c7b3829e70d5f72d
Jun Liu <junl.scholar@gmail.com>
2025-03-18 19:02:48 +0800

    Create __init__.py

---
+++ pplm_contact/__init__.py
@@ -0,0 +1,6 @@
+# from .model import PPLM_Contact
+# from .Module import *
+# from .utils import extract_seq_and_dist_map, pairing_msa, RBF
+# from .LoadHHM import load_hmm
+# from .config import *
+# from .predict import extract_MSA_features, get_pplm_features, collect_all_features, predict_contact
