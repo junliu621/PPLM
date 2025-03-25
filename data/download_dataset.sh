988e353c089c4c45a703191b1195d999c7c5506d
Jun Liu <junl.scholar@gmail.com>
2025-03-25 22:08:54 +0800

    Update and rename README to download_dataset.sh

---
+++ data/download_dataset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Download datasets from PPLM webserver (https://zhanglab.comp.nus.edu.sg/PPLM/benchmark.html)
+
+# Download PPLM dataset
+wget https://zhanglab.comp.nus.edu.sg/PPLM/bin/datasets/PPLM_Dataset.tar.gz
+
+# Download PPLM-Contact dataset
+wget https://zhanglab.comp.nus.edu.sg/PPLM/bin/datasets/PPLM-Contact_Dataset.tar.gz
+
+# Download PPLM-PPI dataset
+wget https://zhanglab.comp.nus.edu.sg/PPLM/bin/datasets/PPLM-PPI_Dataset.tar.gz
