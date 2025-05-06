261f4ec14fad227a461f3c1d8f25692092b39692
Jun Liu <junl.scholar@gmail.com>
2025-05-06 11:46:18 +0800

    Update download_dataset.sh

--- data/download_dataset.sh
+++ data/download_dataset.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Download datasets from PPLM webserver (https://zhanglab.comp.nus.edu.sg/PPLM/benchmark.html)
+# Download datasets from PPLM webserver (https://zhanggroup.org/PPLM/benchmark.html)
 
 # Download PPLM dataset
-wget https://zhanglab.comp.nus.edu.sg/PPLM/bin/datasets/PPLM_Dataset.tar.gz
+wget https://zhanggroup.org/PPLM/bin/datasets/PPLM_Dataset.tar.gz
 
 # Download PPLM-Contact dataset
-wget https://zhanglab.comp.nus.edu.sg/PPLM/bin/datasets/PPLM-Contact_Dataset.tar.gz
+wget https://zhanggroup.org/PPLM/bin/datasets/PPLM-Contact_Dataset.tar.gz
 
 # Download PPLM-PPI dataset
-wget https://zhanglab.comp.nus.edu.sg/PPLM/bin/datasets/PPLM-PPI_Dataset.tar.gz
+wget https://zhanggroup.org/PPLM/bin/datasets/PPLM-PPI_Dataset.tar.gz
