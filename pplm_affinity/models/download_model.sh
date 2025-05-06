6f4e06df5118c5d3cc7e38e0d48e0bfa0d1b11d3
Jun Liu <junl.scholar@gmail.com>
2025-05-06 11:44:58 +0800

    Update download_model.sh

--- pplm_affinity/models/download_model.sh
+++ pplm_affinity/models/download_model.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://zhanglab.comp.nus.edu.sg/PPLM/bin/codes/pplm-affinity_parameters.zip
+wget https://zhanggroup.org/PPLM/bin/codes/pplm-affinity_parameters.zip
 
 unzip pplm-affinity_parameters.zip
