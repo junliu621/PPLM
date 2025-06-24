df59583920c7558b410a11a5d599691914013b26
Jun Liu <junl.scholar@gmail.com>
2025-06-24 16:42:59 +0800

    Update config.py

--- pplm_contact/config.py
+++ pplm_contact/config.py
@@ -1,16 +1,15 @@
 import os
 
-hhsuite_dir = "/mnt/rna01/junl/tools/hh-suite/build"
+hhsuite_dir = "/XXX/hh-suite/build"
 
 hhblits = os.path.join(hhsuite_dir, "bin/hhblits")
 hhmake = os.path.join(hhsuite_dir, "bin/hhmake")
 reformat = os.path.join(hhsuite_dir, "scripts/reformat.pl")
 hhfilter = os.path.join(hhsuite_dir, "bin/hhfilter")
-UniRef_database = "/mnt/dna01/library2/e2e_folding/alphafold/v2.3/lib/uniref30/UniRef30_2021_03"
 
+UniRef_database = "/XXX/uniref30/UniRef30_2021_03"
+esm_msa_model = "/XXX/esm_msa1_t12_100M_UR50S.pt"
 
-ccmpred = "/mnt/rna01/junl/PPLM/PPLM_source_code/pplm_contact/external_tools/ccmpred"
-esm_msa = "/mnt/rna01/junl/PPLM/PPLM_source_code/pplm_contact/external_tools/extract_esm_msa_features.py"
-esm_msa_model = "/mnt/rna01/junl/tools/esm-main/esm_models/esm_msa1_t12_100M_UR50S.pt"
-
-
+pplm_contact_dir = os.path.dirname(os.path.abspath(__file__))
+ccmpred = os.path.join(pplm_contact_dir, "external_tools/ccmpred")
+esm_msa = os.path.join(pplm_contact_dir, "external_tools/extract_esm_msa_features.py")
