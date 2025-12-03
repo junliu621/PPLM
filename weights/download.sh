cb6d2a0910565872d3dea14e5a8d36059ae0c64d
Jun Liu <junl.scholar@gmail.com>
2025-12-03 20:44:28 +0800

    Fix shebang and add gdown commands for downloads
    Updated the shebang line and added multiple gdown commands to download files.

--- weights/download.sh
+++ weights/download.sh
@@ -1,1 +1,16 @@
-#bin/bash
+#!/bin/bash
+
+# PPLM
+gdown --fuzzy https://drive.google.com/file/d/1teZBp3m_OQ4nciTmiepUDP8p9BXoVEwa/view?usp=drive_link
+
+# PPLM-PPI
+gdown --fuzzy https://drive.google.com/file/d/1Xdb3SG0CRY49WqH4jUJhM-yqsLOejz7_/view?usp=share_link
+
+# PPLM-Affinity
+gdown --fuzzy https://drive.google.com/file/d/1s99QyTYjngRUUpy8VXJPhTXYHwmw9agp/view?usp=share_link
+
+# PPLM-Contact
+gdown --fuzzy https://drive.google.com/file/d/1QxSFXojCQmLzgrTz398lUEdIZmqVVV9E/view?usp=share_link
+
+# PPLM-Contact2
+gdown --fuzzy https://drive.google.com/file/d/1SSEkfyiwtUVO4ZSN10HC5T2v-EGOHXle/view?usp=share_link
