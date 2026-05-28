d4479f68a7ba054f733daf603ee2dd51c470f700
Jun Liu <junl.scholar@gmail.com>
2026-05-28 10:26:49 +0800

    Update download_dataset.sh

--- data/download_dataset.sh
+++ data/download_dataset.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
 
-# Download datasets from PPLM webserver (https://zhanggroup.org/PPLM/benchmark.html)
-
-# Download PPLM dataset
-wget https://zhanggroup.org/PPLM/bin/datasets/PPLM_Dataset.tar.gz
-
-# Download PPLM-PPI dataset
-wget https://zhanggroup.org/PPLM/bin/datasets/PPLM-PPI_Dataset.tar.gz
-
-# Download PPLM-Contact dataset
-wget https://zhanggroup.org/PPLM/bin/datasets/PPLM-Contact_Dataset.tar.gz
-
-# Access the binding affinity dataset used in PPLM-Affinity (https://github.com/ChenPy00/PPB-Affinity)
